66a2f301a20f4e613f2e0af51437ad86a49058a6
Gonzalo <gonzalobillia@gmail.com>
2025-06-27 17:01:25 -0300

    realizando cambios para conexion de modulos

--- src/context/Regla/infrastructure/regla.module.ts
+++ src/context/Regla/infrastructure/regla.module.ts
@@ -1,24 +1,24 @@
-// src/context/Puntos/infrastructure/ReglaEngineInfrastructureModule.ts
 import { Module, Provider } from '@nestjs/common';
-import { ReglaEngineServiceInMemory } from './services/ReglaEngineServiceInMemory';
-import { ExecuteRulesUseCase } from '../application/use-cases/ProcessRules';
-import { RulesOrchestrationService } from '../application/services/RuleOrchestationService';
 import { ReglaEngine } from '../core/interfaces/IReglaEngine';
+import { ReglaEngineServiceInMemory } from './services/ReglaEngineServiceInMemory';
+import { RulesOrchestrationService } from '../application/services/RuleOrchestationService';
+import { ExecuteRulesUseCase } from '../application/use-cases/ProcessRules';
 import { REGLA_REPO } from './tokens/tokens';
 import { ReglaRepository } from '../core/repository/ReglaRepository';
 
 const providers: Provider[] = [
-  // 1) Repositorio de reglas in-memory
+  // 1) Stub in-memory del motor de reglas
   { provide: ReglaEngine, useClass: ReglaEngineServiceInMemory },
+  { provide: ReglaRepository, useClass: /**AQUI VA LA IMPL DE TYPE ORM */},
 
-  // 2) Servicio de orquestación que usa el repositorio
+  // 2) Servicio de orquestación que implementa ReglaEngine
   {
-    provide: ReglaEngine,
-    useFactory: (repo: ReglaRepository) => new RulesOrchestrationService(repo),
-    inject: [REGLA_REPO],
+    provide: ReglaRepository,
+    useFactory: (reglaRepo: ReglaRepository) => new RulesOrchestrationService(reglaRepo),
+    inject: [ReglaRepository],
   },
 
-  // 3) Use-case para ejecutar reglas
+  // 3) Caso de uso que delega al servicio de orquestación
   {
     provide: ExecuteRulesUseCase,
     useFactory: (orch: RulesOrchestrationService) =>
