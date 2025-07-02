3e2a546fcbe847b4d2387b3b4c4cc4be07154b6e
Gonzalo <gonzalobillia@gmail.com>
2025-07-02 10:57:00 -0300

    ajuste en package.json

--- src/context/Cliente/application/use-cases/CategoriaCreate/CategoriaCreate.ts
+++ src/context/Cliente/application/use-cases/CategoriaCreate/CategoriaCreate.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 // @cliente/application/use-cases/CategoriaCreate.ts
 import { CategoriaRepository } from '@cliente/core/repository/CategoriaRepository';
 import { CategoriaId } from '@cliente/core/value-objects/CategoriaId';
